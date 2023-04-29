783b034c07e77f5cf3c8f6a3d6a5e63c4e18873c
FishingHacks <fishinghacks@proton.me>
2023-04-29 13:10:04 +0200

    moved to rainbow32 font

--- tooling/package.ts
+++ tooling/package.ts
@@ -1,11 +1,17 @@
 import { readFile, writeFile } from 'fs/promises';
 import { join } from 'path';
+import { watch } from 'fs';
+import * as esbuild from 'esbuild';
 
 async function makeDataURI(file: string, type: string) {
     return `data:${type};base64,${(await readFile(file)).toString('base64')}`;
 }
 
-(async function () {
+let building = false;
+async function build() {
+    if (building) return;
+    console.log('Started build...');
+    building = true;
     let html = (
         await readFile(join(__dirname, 'public', 'index.html'))
     ).toString();
@@ -16,6 +22,20 @@ async function makeDataURI(file: string, type: string) {
             './pixeloid.ttf',
             await makeDataURI(
                 join(__dirname, 'public', 'pixeloid.ttf'),
+                'font/ttf'
+            )
+        )
+        .replaceAll(
+            './rainbow32-font.ttf',
+            await makeDataURI(
+                join(__dirname, 'public', 'rainbow32-font.ttf'),
+                'font/ttf'
+            )
+        )
+        .replaceAll(
+            './rainbow32-no-special-characters.ttf',
+            await makeDataURI(
+                join(__dirname, 'public', 'rainbow32-no-special-characters.ttf'),
                 'font/ttf'
             )
         );
@@ -32,7 +52,7 @@ async function makeDataURI(file: string, type: string) {
             head +
             '</head>' +
             html +
-            `<script>globalThis.__wasmpath = "https://unpkg.com/esbuild-wasm/esbuild.wasm";
+            `<script>globalThis.__wasmpath = "https://unpkg.com/esbuild-wasm@0.17.18/esbuild.wasm";
     
     ${(await readFile(join(__dirname, 'public', 'dist', 'index.js')))
         .toString()
@@ -40,4 +60,26 @@ async function makeDataURI(file: string, type: string) {
     </script></body>` +
             '</html>'
     );
-})();
+    building = false;
+    console.log('Finished build!');
+}
+
+watch(join(__dirname, 'public', 'index.html'), null, build);
+watch(join(__dirname, 'public', 'styles.css'), null, build);
+const packagePlugin = {
+    name: 'packager',
+    setup(_build) {
+        _build.onEnd((result) => {
+            if (result.errors.length < 1) build();
+        });
+    },
+};
+esbuild
+    .context({
+        bundle: true,
+        minify: true,
+        outdir: join(__dirname, 'public', 'dist'),
+        entryPoints: [join(__dirname, 'src', 'index.ts')],
+        plugins: [packagePlugin],
+    })
+    .then((l) => l.watch());
--- tooling/src/esbuild.ts
+++ tooling/src/esbuild.ts
@@ -20,7 +20,7 @@ function stringify(obj1deep: Record<string, any>) {
 
     for (const [k, v] of Object.entries(obj1deep)) {
         str += `${JSON.stringify(k)}: `;
-        if (v instanceof Uint8Array)
+        if (Array.isArray(v) || v instanceof Array)
             str += 'new Uint8Array(' + JSON.stringify([...v.values()]) + ')';
         else if (typeof v === 'object' && v) str += stringify(v);
         else str += JSON.stringify(v);
@@ -71,10 +71,10 @@ export async function compile(code: string) {
         try {
             const parsed = parseAudio(stringAudios[k]);
             audios[k] = {
-                channel1Instrument: parsed.channel1Instrument,
-                channel2Instrument: parsed.channel2Instrument,
-                channel3Instrument: parsed.channel3Instrument,
-                channel4Instrument: parsed.channel4Instrument,
+                channel1instrument: parsed.channel1instrument,
+                channel2instrument: parsed.channel2instrument,
+                channel3instrument: parsed.channel3instrument,
+                channel4instrument: parsed.channel4instrument,
                 length: parsed.length,
                 channel1: [...parsed.channel1.values()],
                 channel2: [...parsed.channel2.values()],
@@ -91,21 +91,69 @@ export async function compile(code: string) {
             audios
         )};const d=(globalThis as any);const e=${JSON.stringify(
             getTexts()
-        )};d.getImage=(path:any)=>a[path];d.getMask=(path:any)=>b[path];d.getAudio=(path:any)=>c[path];d.getString=(path:any)=>e[path];})();\n\n${code}`
+        )};d.getimage=(path:any)=>a[path];d.getmask=(path:any)=>b[path];d.getaudio=(path:any)=>c[path];d.getstring=(path:any)=>e[path];})();\n${code}`
     ).catch((err) => {
-        console.error(err);
-        createNotification(
-            'Error',
-            'Failed to compile! Check the console (F12/Ctrl+Shift+I)',
-            '#b91c1c'
+        globalThis.err = err;
+        let msg = '' + err;
+        msg = msg.replaceAll(
+            /<stdin>:([0-9]+):([0-9]+)/g,
+            (
+                _str: string,
+                ln: string,
+                char: string,
+            ) => {
+                return 'game.ts:' + (Number(ln) - 1) + ':' + char;
+            }
         );
-        return;
+
+        console.error(msg);
+        return compileTypescript(`(function() {
+        const colors = [3, 4, 5, 27, 28, 12, 13, 11, 10, 9, 15, 19, 20];
+        function writeRainbowText(x: number, text: string, y: number) {
+            let col = 0;
+            for (let i = 0; i < text.length; ++i) {
+                x =
+                    TextUtils.writeText(text[i], x, y, WIDTH, {
+                        color: colors[col % colors.length],
+                        background: 0,
+                    })[0].end + 1;
+                if (text[i] === '\\n') y += 6;
+                if (text[i] !== '\\n' && text[i] !== ' ') col++;
+            }
+        
+            return { x, y };
+        };
+    __registeredGame={name: 'failed to compile', bg: '#000', 
+        async init() {
+            imageUtils.cls();
+            imageUtils.putImage(0, 0, imageUtils.square(WIDTH, HEIGHT, 0));
+            const y = writeRainbowText(3, 'Rainbow32 V1.0', 3).y + 8;
+            TextUtils.writeText("Cartridge Crashed! :(\\n\\n" + ${JSON.stringify(
+                msg
+            )}, 3, y, WIDTH - 3, {
+                color: 3,
+                background: 0,
+            });
+        
+            await audioUtils.playSound(
+                { octave: 4, halfToneStepUp: false, sound: 'c' },
+                'square-wave',
+                0.2,
+                0.1
+            );
+            await audioUtils.playSound(
+                { octave: 3, halfToneStepUp: false, sound: 'c' },
+                'square-wave',
+                0.2,
+                0.2
+            );
+        }}})();`);
     });
 }
 
 export async function compileTypescript(code: string): Promise<string> {
     await promise;
-    const res = await esbuild.transform(globalcode + '\n\n' + code, {
+    const res = await esbuild.transform(globalcode + ';' + code, {
         minify: true,
         loader: 'ts',
     });
