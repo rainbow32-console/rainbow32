d449906c25fda3942f24323c61f3d3a40015888c
FishingHacks <fishinghacks@proton.me>
2023-05-10 19:06:05 +0200

    added colornames

---
+++ rainbow32/src/namespacedcolors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+    black: 0,
+    white: 20,
+    whitish: 3,
+    red: 9,
+    brightred: 26,
+    darkred: 8,
+    lightgray: 21,
+    gray: 22,
+    darkgray: 23,
+} as const;
