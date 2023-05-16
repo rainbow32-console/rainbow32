9efdbece05e962ca9adf8c175ccc425459a5d3e0
FishingHacks <fishinghacks@proton.me>
2023-05-16 12:08:12 +0200

    (rainbow32) rendering is now more efficient

--- rainbow32/src/components/imageRenderer.ts
+++ rainbow32/src/components/imageRenderer.ts
@@ -1,23 +1,38 @@
-import { getAnimationFrame } from '../animation';
+import { AnimationPlayer } from '../animation';
 import { component, gameobject } from '../gameObject';
 import { applyImageMask, putImage } from '../imageUtils';
+import type { Image, ImageMask } from '../imageUtils';
 
 interface props {
-    start: number;
+    mask?: AnimationPlayer<ImageMask>;
+    img?: AnimationPlayer<Image>;
 }
 
 class imageRenderer implements component<props> {
     readonly name = 'imagerenderer';
-    init(){return {start: Date.now()}}
+    init(_cfg: Partial<props>, obj: gameobject) {
+        return {
+            mask: Array.isArray(obj.mask)
+                ? new AnimationPlayer(obj.mask)
+                : undefined,
+            image: Array.isArray(obj.image)
+                ? new AnimationPlayer(obj.image)
+                : undefined,
+        };
+    }
     update(cfg: props, dt: number, gameObject: gameobject) {
-        let image = Array.isArray(gameObject.image) ? getAnimationFrame(gameObject.image, Date.now()-cfg.start) : gameObject.image;
-        let mask = Array.isArray(gameObject.mask) ? getAnimationFrame(gameObject.mask, Date.now()-cfg.start) : gameObject.mask;
-        if (!image || image.width < 1 || image.height < 1) return;
-        if (
-            mask &&
-            mask.width === image.width &&
-            mask.height === image.height
-        )
+        if (Array.isArray(gameObject.image) && !cfg.img)
+            cfg.img = new AnimationPlayer(gameObject.image);
+        if (Array.isArray(gameObject.mask) && !cfg.mask)
+            cfg.mask = new AnimationPlayer(gameObject.mask);
+        if (cfg.img && !cfg.img.isplaying) cfg.img.play();
+        if (cfg.mask && !cfg.mask.isplaying) cfg.mask.play();
+        let image = cfg.img ? cfg.img.getframe()! : (gameObject.image as Image);
+        let mask = cfg.mask
+            ? cfg.mask.getframe()
+            : (gameObject.mask as ImageMask | undefined);
+
+        if (mask && mask.width === image.width && mask.height === image.height)
             image = applyImageMask(image, mask);
 
         putImage(
